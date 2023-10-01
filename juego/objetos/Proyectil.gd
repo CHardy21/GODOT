7e7a8b2b3b7281816811be88580e80059c7dbcda
Christian H <christianrh2000@hotmail.com>
2023-10-01 17:24:16 -0300

    txt

--- juego/objetos/Proyectil.gd
+++ juego/objetos/Proyectil.gd
@@ -21,19 +21,12 @@ func _on_VisibilityNotifier2D_screen_exited():
 
 
 func _on_Proyectil_area_entered(area):
-	print("impacte en algo (area) -> ", area.get_parent().name)
+#	print("impacte en algo (area) -> ", area.get_parent().name)
 	if area.owner.is_in_group("Enemy") or area.is_in_group("Enemy") :
-		print("Impacte en enemigo...", area.owner.name)
+#		print("Impacte en enemigo...", area.owner.name)
 		# Aqui se podria poner algun efecto de explosion
 		# y luego de finalizado el efecto llamar recien llamar a queue_free()
 		destruir_proyectil()
-
-
-#func _on_Proyectil_body_entered(body):
-#	if body.owner.is_in_group("Enemy"):
-#		print("Impacte en enemigo...", body.name)
-#		destruir_proyectil()
-#	print("Impacte con algo (body)", body.name)
 
 
 func destruir_proyectil():
